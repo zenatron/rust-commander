68f6752c0885199949ea93ae17036d8bbe45f93a
Phil Vishnevsky <70730887+zenatron@users.noreply.github.com>
2025-05-20 10:01:47 -0400

    path issue

--- scripts/rename-release-windows.ps1
+++ scripts/rename-release-windows.ps1
@@ -1,8 +1,11 @@
 $metadata = cargo metadata --no-deps --format-version 1 | ConvertFrom-Json
 $version = $metadata.packages[0].version
-$src = "target\x86_64-pc-windows-msvc\release\rust-commander.exe"
-$dest = "target\x86_64-pc-windows-msvc\release\rust-commander-win-$version.exe"
 
+# Define paths using Join-Path
+$src = Join-Path "target\x86_64-pc-windows-msvc\release" "rust-commander.exe"
+$dest = Join-Path "target\x86_64-pc-windows-msvc\release" ("rust-commander-win-$version.exe")
+
+# Check if the source file exists and rename it
 if (Test-Path $src) {
     Rename-Item -Path $src -NewName $dest -Force
     Write-Host "Renamed to: $dest"
