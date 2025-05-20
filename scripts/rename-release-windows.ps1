8f158e8cd0caf5f576f9116b7049f237eeba6ff5
Phil Vishnevsky <70730887+zenatron@users.noreply.github.com>
2025-05-20 10:09:22 -0400

    update scripts

--- scripts/rename-release-windows.ps1
+++ scripts/rename-release-windows.ps1
@@ -1,13 +1,18 @@
 $metadata = cargo metadata --no-deps --format-version 1 | ConvertFrom-Json
 $version = $metadata.packages[0].version
 
-# Define paths using Join-Path
-$src = Join-Path "target\x86_64-pc-windows-msvc\release" "rust-commander.exe"
-$dest = Join-Path "target\x86_64-pc-windows-msvc\release" ("rust-commander-win-$version.exe")
+# Define paths
+$srcDir = "target\\x86_64-pc-windows-msvc\\release"
+$srcFile = "rust-commander.exe"
+$src = Join-Path $srcDir $srcFile
+
+$newFileName = "rust-commander-win-$version.exe"
+# Construct the full destination path for logging purposes
+$dest = Join-Path $srcDir $newFileName
 
 # Check if the source file exists and rename it
 if (Test-Path $src) {
-    Rename-Item -Path $src -NewName $dest -Force
+    Rename-Item -Path $src -NewName $newFileName -Force
     Write-Host "Renamed to: $dest"
 } else {
     Write-Host "File not found: $src"
